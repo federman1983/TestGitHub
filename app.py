dd27a6e6d77687c324887fe81875c2e2a5f0a532
federman1983 <federman.sergey@gmail.com>
2025-04-06 19:45:16 +0300

    bla bla bla

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+class Base:
+    def display_something():
+        print("Kokokokooo")
+
+
 print("Hello World")
 print("My name is Sergey")
+Base.display_something()
+
 #git add.
 #git commit -m"bla bla bla"
 #git push origin main
